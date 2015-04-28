8ba849404d5342cfcdaa61818dbb497cbf74f7a8
Ted <tedstirm@centralite.com>
2015-04-28 14:29:50 -0500

    Added a new url, view and template to display lists, instead of just having home url do this and the post as well

--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,21 @@ from lists.views import home_page
 from lists.models import Item
 
 # Create your tests here.
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+        
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -39,22 +54,14 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+    
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 from lists.models import Item
 
 # Create your views here.
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items':items})
+
 def home_page(request): 
     if request.method == 'POST':        
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items':items})
+    return render(request, 'home.html')

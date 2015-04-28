a8e0ce6da5f9376f9db4747796841e521d888e67
Ted <tedstirm@centralite.com>
2015-04-28 15:08:25 -0500

    Added list model so that items can be aggregated together.

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):

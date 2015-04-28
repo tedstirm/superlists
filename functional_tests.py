e875ab4c40bc800d78d35024fd10f117f1300a3e
Ted <tedstirm@centralite.com>
2015-04-28 10:02:12 -0500

    Refactored functional test to reduce code duplication.

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_Retrieve_it_later(self):
 
 		# Edith has heard about a cool new online to-do app. 
@@ -30,28 +35,29 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lurers)
-		inputbox.send_keys('Buy peacock feathres')
+		inputbox.send_keys('Buy peacock feathers')
 
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacok feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-						"New to-do item did not appear in table")
+		self.check_for_row_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item.
 		# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both items on her list
+		self.check_for_row_list_table('1: Buy peacock feathers')
+		self.check_for_row_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith wonders whether the site will remember her list. Then she sees 
 		# the site has generated a unique URL for her -- there is some 
 		# explanatory text to that effect.
-
+		self.fail('Finish the test!')
+		
 		# She visits that URL - her to-do list is still there.
 
 		# Satisified, she goes back to sleep
--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home_page(request):
-	return render(request, 'home.html')
+def home_page(request):	
+	return render(request, 'home.html', 
+				 {'new_item_text': request.POST.get('item_text', ''),})

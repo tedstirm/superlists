55de8d1eda1c4882b6ab74071ccda15b6ff52a75
Ted <tedstirm@centralite.com>
2015-04-30 09:23:15 -0500

    new url + view for adding to existing lists. All test passing!

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -45,13 +45,13 @@ class NewVisitorTest(LiveServerTestCase):
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+') #1
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-
+        
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-
+        
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -90,7 +90,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees 
         # the site has generated a unique URL for her -- there is some 
         # explanatory text to that effect.
-        self.fail('Finish the test!')
+        
         
         # She visits that URL - her to-do list is still there.
 
